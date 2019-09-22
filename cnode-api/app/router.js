598d3c20f789db636129e78e31dbb46ab3e4cbcf
lazycode <wkl13022195532@gmail.com>
2019-09-22 16:45:15 +0800

    add code

--- cnode-api/app/router.js
+++ cnode-api/app/router.js
@@ -3,4 +3,5 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/demo', controller.demo.show);
+  router.get('/user', controller.user.login);
 };
--- cnode-api/config/plugin.js
+++ cnode-api/config/plugin.js
@@ -4,3 +4,8 @@ exports.validate = {
   enable: true,
   package: 'egg-validate',
 };
+
+exports.mysql = {
+  enable: true,
+  package: 'egg-mysql',
+};
