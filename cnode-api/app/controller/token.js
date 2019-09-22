3c1556d4101449ca126eb3ae48692c663ab84fbe
lazycode <wkl13022195532@gmail.com>
2019-09-22 21:28:16 +0800

    add code

--- cnode-api/app/controller/token.js
+++ cnode-api/app/controller/token.js
@@ -7,7 +7,7 @@ class TokenController extends Controller {
   async index() {
     const { ctx,app } = this;
     const token = app.jwt.sign({ foo: 'bar', iat: Math.floor(Date.now() / 1000) - 30  }, app.config.jwt.secret);
-    ctx.body = token
+    ctx.body ='Bearer '+token
   }
 }
 module.exports =TokenController
