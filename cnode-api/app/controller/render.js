cb90997a94e14667fa6e5a8e2ea6510ca9ba8e56
lazycode <wkl13022195532@gmail.com>
2019-09-23 14:58:41 +0800

    add code

--- cnode-api/app/controller/render.js
+++ cnode-api/app/controller/render.js
@@ -5,10 +5,22 @@ const Controller = require('egg').Controller;
 class RenderController extends Controller {
 
   async index() {
-    const { ctx } = this;
-
+    const { ctx,app } = this;
+    const token = ctx.header.authorization  // 获取jwt
+    let payload
+    if (token) {
+        payload = await app.jwt.verify(token.split(' ')[1], secret)  // // 解密，获取payload
+        ctx.body = {
+            payload
+        }
+    } else {
+        ctx.body = {
+            message: 'token 错误',
+            code: -1
+        }
+    }
    
-    ctx.body = 'fklnerkfn'
+    // ctx.body = 'fklnerkfn'
   }
 }
 module.exports =RenderController
